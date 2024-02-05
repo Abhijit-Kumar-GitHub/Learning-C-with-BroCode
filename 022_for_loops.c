08fbc1fa856f811d0884b57f2bd6dda0e14b93ed
Abhijit <abhijitabhi127@gmail.com>
2024-02-05 16:43:50 +0530

    completed for loops

---
+++ 022_for_loops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    // for loop = repeats a section of a code for a speecific amount of times
+
+    for(int index = 1; index <=10; index++){
+        printf("%d\n", index);
+    }
+
+    /*
+    for(declaration and initialization of controller/index; condtion till which to repeat; when when to perfor loop){
+        body of loop containing code which is to be iterated
+    }
+    */
+
+    return 0;
+}
